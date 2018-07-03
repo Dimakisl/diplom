ba0c46cac384f264543acee628ee6c61290e67ec
dimbos <dimboss@list.ru>
2018-07-04 00:39:54 +0300

    pravka 8

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 window.addEventListener('DOMContentLoaded', () =>{
 
+// переменные для калькулятора
+let calcBtn = document.querySelectorAll('.popup_calc_btn'); //кнопка вызова калькулятора
+let popCalc = document.querySelectorAll('.popup_calc')[0]; // окно калькудятора
+let popCalcClose = document.querySelector('.popup_calc_close'); //закрытие окна калькулятора
+let calcNext = popCalc.querySelector('.popup_calc_button'); // кнопка "далее" у  калькулятора
+let calcNextProfile = document.querySelector('.popup_calc_profile'); // следующее окно за калькулятором
+let calcNextEnd = document.querySelector('.popup_calc_profile_button'); //кнопка далее на последнюю страницу
+let calcEnd = document.querySelector('.popup_calc_end'); //последнее окно калькулятора
+let calcCloseEnd = document.querySelector('.popup_calc_end_close');
+
+
+
 //modal Request an engineer
 function zamer(){
 let popupEngener = document.getElementsByClassName('popup_engineer')[0];
 let popupBtn = document.getElementsByClassName('popup_engineer_btn')[0];
 let popupClose  = document.getElementsByClassName('popup_close')[1];
+
+//для Хранения данных пользователя в переменных
+let width = 0;
+let height = 0;
+let check1; //храним условие "холодное"
+let check2; // храним условие "теплое"
+let view; //храним вид балкона
 
 
 
@@ -183,7 +202,7 @@ tabHeadero.addEventListener('click', (e) =>{
 let message = new Object();
 message.loading = 'Загрузка ...';
 message.success = 'Спасибо! Скоро мы с вами свяжемся!';
-message.failure = 'Что-то пощло не так...';
+message.failure = 'Что-то пошло не так...';
 
 let form = document.getElementsByClassName('main_form'),
     
@@ -366,6 +385,135 @@ setTimeout(() =>{
     popupZvonok.style.display = 'block';
 }, 1000);
 
+
+//calc
+
+for(let i = 0; i < calcBtn.length; i++)
+{
+    calcBtn[i].addEventListener('click', calc);
+};
+
+function calc(){
+
+    //объект для хранения данных калькулятора 
+let answer = new Object();
+
+    popCalc.style.display = 'block'; //показываем окно калькулятора
+
+    //по клику закрываем окно
+    popCalcClose.addEventListener('click', () =>{
+        popCalc.style.display = 'none';
+    })
+
+    //переключение балконов
+    let tabHeaderw = document.querySelector('.info-header-tab_w'); //блок с табами
+let tabw = document.getElementsByClassName('tab_w');//таб на которые щелкнули
+//let tabAo = document.getElementsByTagName('after_click');
+let tabContentw = document.getElementsByClassName('info-tabcontent_w'); //контент таба
+
+
+
+//скрываем табs балконов
+function hideTabContentw(a){
+    for(let i = a; i < tabContentw.length; i++){
+
+        tabContentw[i].classList.remove('show');
+        tabContentw[i].classList.add('hide');
+
+    }
+};
+
+hideTabContentw(1);
+
+function showTabContentw(b){
+    if(tabContentw[b].classList.contains('hide')){
+        hideTabContentw(0);
+        tabContentw[b].classList.remove('hide');
+        tabContentw[b].classList.add('show');
+    }
+}
+
+tabHeaderw.addEventListener('click', function(e){
+    e.preventDefault();
+    let target = e.target;
+        for(let i = 0; i < tabw.length; i++){
+            if(target == tabw[i]){
+                showTabContentw(i);
+                answer.view = tabw[i].getAttribute('data-view');
+                break;
+            }
+        }
+});
+
+let calcInput = popCalc.querySelectorAll('.form-control');
+
+for(let i = 0; i < calcInput.length; i++){
+    calcInput[i].onkeyup = function (){
+        return this.value = this.value.replace(/[^\d]/g, '');
+    }
+}
+
+calcNext.addEventListener('click', () =>{
+    popCalc.style.display = 'none';
+    calcNextProfile.style.display = 'block';
+
+    width = calcInput[0].value;
+    height = calcInput[1].value;
+
+    answer.width = +width;
+    answer.height = +height;
+
+
+    calcNextProfile.querySelector('.popup_calc_profile_close').addEventListener('click', ()=>{
+    calcNextProfile.style.display = 'none';
+    });
+
+    check1 = calcNextProfile.querySelectorAll('.checkbox')[0];
+    check2 = calcNextProfile.querySelectorAll('.checkbox')[1];
+
+    
+
+    calcNextEnd.setAttribute('disabled', 'disabled'); //отключаем кнопку "далее" пока не будет поставлена галка
+
+   check1.addEventListener('change', () => {
+       check2.checked = false;
+       calcNextEnd.disabled = false;
+       answer.condition = check1.value;
+   })
+  
+    check2.addEventListener('change', () => {
+        check1.checked = false;
+        calcNextEnd.disabled = false;
+        answer.condition = check2.value;   
+    });
+
+   
+
+});
+
+
+calcNextEnd.addEventListener('click', () => {
+    alert( `Ширина:  + ${answer.width} высота: ${answer.height} Условия: ${answer.condition} и типа: ${answer.view}`);
+    calcNextProfile.style.display = 'none';
+    calcEnd.style.display = 'block';
+    
+    });
+
+    calcCloseEnd.addEventListener('click', () =>{
+        calcEnd.style.display = 'none';
+        answer.length = 0; //чистим объект с ответами
+
+        //чистим поля
+        for(let i = 0; i < calcInput.length; i++){
+            calcInput[i].value = '';
+        }
+        check1.checked = false;
+        check2.checked = false;
+        
+        
+    });
+
+}
 
 
 });
